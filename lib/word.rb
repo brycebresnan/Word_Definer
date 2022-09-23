73d94360d3f8786f9959bdad6280012a9c7b30e9
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-09-22 22:47:49 -0700

    GREEN: add word class, init and #list method

---
+++ lib/word.rb
@@ -0,0 +1,21 @@
+class Word
+  attr_reader :id
+  attr_accessor :word
+
+  @@words = {}
+  @@unique_id = 0
+
+  def initialize(attributes)
+    @word = attributes.fetch(:word)
+    @id = attributes.fetch(:id) || @@unique_id =+ 1
+  end
+
+  def self.list
+    @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new(word: self.word, id: self.id)
+  end
+
+end
