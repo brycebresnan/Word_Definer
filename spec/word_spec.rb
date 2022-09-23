5fff1a640ce55eda6d051d6c257d1fcf8832e96f
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-09-22 22:48:08 -0700

    RED: #save method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,11 +10,18 @@ describe '#Word' do
       id: nil
     }
 
+    word = Word.new(attributes)
   end
 
   describe('.list') do
     it("returns an array of the words saved in the hash. Will return empty array if nothing is stored.") do
       expect(Word.list).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("saves a word to the class varriable hash") do
+      expect(Word.list).to(eq([word]))
     end
   end
 
