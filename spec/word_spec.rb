03fa3d01df16d54f0e0380072e805eb46ed315a4
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-09-22 22:52:52 -0700

    RED: added method needed to past previous method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,12 +5,13 @@ describe '#Word' do
 
   before(:each) do
 
-    attributes = {
+    @attributes = {
       word: "Cantankerous",
       id: nil
     }
 
-    word = Word.new(attributes)
+    @word = Word.new(@attributes)
+
   end
 
   describe('.list') do
@@ -19,10 +20,18 @@ describe '#Word' do
     end
   end
 
-  describe('#save') do
-    it("saves a word to the class varriable hash") do
-      expect(Word.list).to(eq([word]))
+  describe('#==') do
+    it("allows test to compare two of the same words ") do
+      word = Word.new(word: "Cantankerous", id: nil)
+      word2 = Word.new(word: "Cantankerous", id: nil)
+      expect(word).to(eq(word2))
     end
   end
+
+  # describe('#save') do
+  #   it("saves a word to the class varriable hash") do
+  #     expect(Word.list).to(eq([@word]))
+  #   end
+  # end
 
 end
