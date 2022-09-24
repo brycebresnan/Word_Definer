456db7b536428ecb8daa4225279f9009d70005b1
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-09-23 20:33:33 -0700

    create new word routing and view

--- app.rb
+++ app.rb
@@ -13,3 +13,10 @@ end
 get('/words/new') do
   erb(:words_new)
 end
+
+post('/words') do
+  new_word = params[:word]
+  word = Word.new(word: new_word, id: nil)
+  word.save()
+  redirect to('/')
+end
